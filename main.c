c0813cb20fb4448410d74db02e60052b85767059
Tim Cameron Ryan <id@timryan.org>
2015-05-05 00:21:54 -0700

    Supports timers.

--- main.c
+++ main.c
@@ -4,38 +4,137 @@
 #include "stm8s.h"
 
 inline void gpio_input (uint8_t pin) {
-	uint8_t value = PD_DDR;
-	PD_DDR = value & ~(1 << pin);
+	uint8_t value = PORT_D->DDR;
+	PORT_D->DDR = value & ~(1 << pin);
 }
 
 inline void gpio_output (uint8_t pin) {
-	uint8_t value = PD_DDR;
-	PD_DDR = value | (1 << pin);
+	uint8_t value = PORT_D->DDR;
+	PORT_D->DDR = value | (1 << pin);
+}
+
+inline uint8_t gpio_read (uint8_t pin) {
+	uint8_t value = PORT_D->ODR;
+	return (value >> pin) & 0x1;
 }
 
 inline void gpio_write (uint8_t pin, uint8_t val) {
-	uint8_t value = PD_ODR;
-	PD_ODR = (value & ~(1 << pin)) | (1 << val);
+	uint8_t value = PORT_D->ODR;
+	PORT_D->ODR = (value & ~(1 << pin)) | (1 << val);
 }
 
-void main (void)
+//
+//  Timer 2 Overflow handler.
+//
+void irq_timer2(void) __interrupt(IRQ_TIM2)
 {
-	uint32_t i;
+	gpio_write(0, gpio_read(0));
+	//  Reset the interrupt otherwise it will fire again straight away.
+	TIM2_SR1->UIF = 0;
+}
 
+//
+//  Reset Timer 2 to a known state.
+//
+void InitialiseTimer2()
+{
+    TIM2_CR1_REG = 0;               // Turn everything TIM2 related off.
+    TIM2_IER_REG = 0;
+    TIM2_SR2 = 0;
+    TIM2_CCER1 = 0;
+    TIM2_CCER2 = 0;
+    TIM2_CCER1 = 0;
+    TIM2_CCER2 = 0;
+    TIM2_CCMR1 = 0;
+    TIM2_CCMR2 = 0;
+    TIM2_CCMR3 = 0;
+    TIM2_CNTRH = 0;
+    TIM2_CNTRL = 0;
+    TIM2_PSCR = 0;
+    TIM2_ARRH  = 0;
+    TIM2_ARRL  = 0;
+    TIM2_CCR1H = 0;
+    TIM2_CCR1L = 0;
+    TIM2_CCR2H = 0;
+    TIM2_CCR2L = 0;
+    TIM2_CCR3H = 0;
+    TIM2_CCR3L = 0;
+    TIM2_SR1_REG = 0;
+}
+
+//
+//  Setup Timer 2 to generate a 20 Hz interrupt based upon a 16 MHz timer.
+//
+void SetupTimer2()
+{
+    TIM2_PSCR = 0x03;       //  Prescaler = 8.
+    TIM2_ARRH = (25000 >> 8);       //  High byte of 50,000.
+    TIM2_ARRL = (25000 & 0xFF);       //  Low byte of 50,000.
+    TIM2_IER->UIE = 1;       //  Enable the update interrupts.
+    TIM2_CR1->CEN = 1;       //  Finally enable the timer.
+}
+
+
+void SetupOutputPorts()
+{
 	gpio_output(0);
 
-	while (1) {
-		gpio_write(0, 0);
-		for (i = 0; i < 1e4; i++) {
-			__asm
-			nop
-			__endasm;
-		}
-		gpio_write(0, 1);
-		for (i = 0; i < 1e4; i++) {
-			__asm
-			nop
-			__endasm;
-		}
-	}
 }
+
+
+inline void __disable_interrupt(void) {
+	__asm
+	sim
+	__endasm;
+}
+
+inline void __enable_interrupt(void) {
+	__asm
+	rim
+	__endasm;
+}
+
+inline void __wait_for_interrupt(void) {
+	__asm
+	wfi
+	__endasm;
+}
+
+//
+//  Main program loop.
+//
+void main (void)
+{
+    __disable_interrupt();
+    SetupOutputPorts();
+    InitialiseTimer2();
+    SetupTimer2();
+    __enable_interrupt();
+    while (1)
+    {
+        __wait_for_interrupt();
+    }
+}
+
+
+// void main (void)
+// {
+// 	uint32_t i;
+
+// 	gpio_output(0);
+
+// 	while (1) {
+// 		gpio_write(0, 0);
+// 		for (i = 0; i < 1e4; i++) {
+// 			__asm
+// 			nop
+// 			__endasm;
+// 		}
+// 		gpio_write(0, 1);
+// 		for (i = 0; i < 1e4; i++) {
+// 			__asm
+// 			nop
+// 			__endasm;
+// 		}
+// 	}
+// }
--- stm8s.h
+++ stm8s.h
@@ -1,34 +1,21 @@
+#define IO_PTR(x)	((volatile uint8_t *)(x))
+#define IO_TYPE(T, x)	((volatile T *)(x))
+
 #define MAP_IO      ((volatile uint8_t *)0x5000)
 
-#define PA_ODR		(MAP_IO[(0*0x5)+0x0])
-#define PA_IDR		(MAP_IO[(0*0x5)+0x1])
-#define PA_DDR		(MAP_IO[(0*0x5)+0x2])
-#define PA_CR1		(MAP_IO[(0*0x5)+0x3])
-#define PA_CR2		(MAP_IO[(0*0x5)+0x4])
+typedef struct {
+	uint8_t ODR;
+	uint8_t IDR;
+	uint8_t DDR;
+	uint8_t CR1;
+	uint8_t CR2;
+} port_t;
 
-#define PB_ODR		(MAP_IO[(1*0x5)+0x0])
-#define PB_IDR		(MAP_IO[(1*0x5)+0x1])
-#define PB_DDR		(MAP_IO[(1*0x5)+0x2])
-#define PB_CR1		(MAP_IO[(1*0x5)+0x3])
-#define PB_CR2		(MAP_IO[(1*0x5)+0x4])
-
-#define PC_ODR		(MAP_IO[(2*0x5)+0x0])
-#define PC_IDR		(MAP_IO[(2*0x5)+0x1])
-#define PC_DDR		(MAP_IO[(2*0x5)+0x2])
-#define PC_CR1		(MAP_IO[(2*0x5)+0x3])
-#define PC_CR2		(MAP_IO[(2*0x5)+0x4])
-
-#define PD_ODR		(MAP_IO[(3*0x5)+0x0])
-#define PD_IDR		(MAP_IO[(3*0x5)+0x1])
-#define PD_DDR		(MAP_IO[(3*0x5)+0x2])
-#define PD_CR1		(MAP_IO[(3*0x5)+0x3])
-#define PD_CR2		(MAP_IO[(3*0x5)+0x4])
-
-#define PE_ODR		(MAP_IO[(4*0x5)+0x0])
-#define PE_IDR		(MAP_IO[(4*0x5)+0x1])
-#define PE_DDR		(MAP_IO[(4*0x5)+0x2])
-#define PE_CR1		(MAP_IO[(4*0x5)+0x3])
-#define PE_CR2		(MAP_IO[(4*0x5)+0x4])
+#define PORT_A      ((volatile port_t *) (0x5000 + 0*0x5))
+#define PORT_B      ((volatile port_t *) (0x5000 + 1*0x5))
+#define PORT_C      ((volatile port_t *) (0x5000 + 2*0x5))
+#define PORT_D      ((volatile port_t *) (0x5000 + 3*0x5))
+#define PORT_E      ((volatile port_t *) (0x5000 + 4*0x5))
 
 #define CLK_DIVR	(*(volatile uint8_t *)0x50c6)
 #define CLK_PCKENR1	(*(volatile uint8_t *)0x50c7)
@@ -41,11 +28,62 @@
 #define USART1_CR2	(*(volatile uint8_t *)0x5235)
 #define USART1_CR3	(*(volatile uint8_t *)0x5236)
 
-#define TIM1_CR1	(*(volatile uint8_t *)0x5250)
-#define TIM1_CNTRH	(*(volatile uint8_t *)0x525e)
-#define TIM1_CNTRL	(*(volatile uint8_t *)0x525f)
-#define TIM1_PSCRH	(*(volatile uint8_t *)0x5260)
-#define TIM1_PSCRL	(*(volatile uint8_t *)0x5261)
+#define TIM1_CR1	(*IO_PTR(0x5250))
+#define TIM1_CNTRH	(*IO_PTR(0x525e))
+#define TIM1_CNTRL	(*IO_PTR(0x525f))
+#define TIM1_PSCRH	(*IO_PTR(0x5260))
+#define TIM1_PSCRL	(*IO_PTR(0x5261))
+
+
+typedef struct {
+	uint8_t CEN:1;
+	uint8_t UDIS:1;
+	uint8_t URS:1;
+	uint8_t OPM:1;
+	uint8_t RESERVED0:3;
+	uint8_t ARPE:1;
+} tim2_cr1_t;
+
+#define TIM2_CR1	IO_TYPE(tim2_cr1_t, 0x5300)
+#define TIM2_CR1_REG	(*IO_PTR(0x5300))
+
+typedef struct {
+	uint8_t UIE:1;
+	uint8_t CC1IE:1;
+	uint8_t CC2IE:1;
+	uint8_t CC3IE:1;
+} tim2_ier_t;
+
+#define TIM2_IER	IO_TYPE(tim2_ier_t, 0x5303)
+#define TIM2_IER_REG	(*IO_PTR(0x5303))
+
+typedef struct {
+	uint8_t UIF:1;
+	uint8_t CC1IF:1;
+	uint8_t CC2IF:1;
+	uint8_t CC3IF:1;
+} tim2_sr1_t;
+
+#define TIM2_SR1	IO_TYPE(tim2_sr1_t, 0x5304)
+#define TIM2_SR1_REG	(*IO_PTR(0x5304))
+#define TIM2_SR2	(*IO_PTR(0x5305))
+#define TIM2_EGR	(*IO_PTR(0x5306))
+#define TIM2_CCMR1	(*IO_PTR(0x5307))
+#define TIM2_CCMR2	(*IO_PTR(0x5308))
+#define TIM2_CCMR3	(*IO_PTR(0x5309))
+#define TIM2_CCER1	(*IO_PTR(0x530A))
+#define TIM2_CCER2	(*IO_PTR(0x530B))
+#define TIM2_CNTRH	(*IO_PTR(0x530C))
+#define TIM2_CNTRL	(*IO_PTR(0x530D))
+#define TIM2_PSCR	(*IO_PTR(0x530E))
+#define TIM2_ARRH	(*IO_PTR(0x530F))
+#define TIM2_ARRL	(*IO_PTR(0x5310))
+#define TIM2_CCR1H	(*IO_PTR(0x5311))
+#define TIM2_CCR1L	(*IO_PTR(0x5312))
+#define TIM2_CCR2H	(*IO_PTR(0x5313))
+#define TIM2_CCR2L	(*IO_PTR(0x5314))
+#define TIM2_CCR3H	(*IO_PTR(0x5315))
+#define TIM2_CCR3L	(*IO_PTR(0x5316))
 
 #define USART_CR2_TEN (1 << 3)
 #define USART_CR3_STOP2 (1 << 5)
@@ -55,4 +93,30 @@
 #define SPI_BLOCK   (*(volatile uint8_t *)0x5200)
 #define SPI_SR      (*((&SPI_BLOCK) + 0x3))
 
-#define IRQ_SPI     10
+typedef enum {
+	IRQ_TLI = 0,
+	IRQ_AWU,
+	IRQ_CLK,
+	IRQ_EXTI0,
+	IRQ_EXTI1,
+	IRQ_EXTI2,
+	IRQ_EXTI3,
+	IRQ_EXTI4,
+	IRQ_RESERVED0,
+	IRQ_RESERVED1,
+	IRQ_SPI,
+	IRQ_TIM1,
+	IRQ_TIM1_COMPARE,
+	IRQ_TIM2,
+	IRQ_TIM2_COMPARE,
+	IRQ_RESERVED2,
+	IRQ_RESERVED3,
+	IRQ_UART1,
+	IRQ_UART1_FULL,
+	IRQ_I2C,
+	IRQ_RESERVED4,
+	IRQ_RESERVED5,
+	IRQ_ADC1,
+	IRQ_TIM4,
+	IRQ_FLASH = 24
+} irq_t;
